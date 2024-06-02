084f761ea91c14e5f787754f0601502d8698e528
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-06-02 17:50:25 +0530

    update

--- CountMaximumConsecutiveOnes.java
+++ CountMaximumConsecutiveOnes.java
@@ -1,6 +1,6 @@
 public class CountMaximumConsecutiveOnes {
     public static void main(String[] args) {
-       int [] arr={1, 1, 0, 1, 1, 1};
+       int [] arr={1,0,1,1,0,1};
        int maxCount=0;
        int count=0;
        for(int i=0;i<arr.length;i++){
